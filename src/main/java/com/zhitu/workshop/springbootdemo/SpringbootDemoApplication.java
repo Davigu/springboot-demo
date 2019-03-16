5d69872dde1b0fb918d01609e669c268cd228d46
顾达伟 <1533592910@qq.com>
2019-03-16 11:46:12 +0800

    0.0

--- src/main/java/com/zhitu/workshop/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/zhitu/workshop/springbootdemo/SpringbootDemoApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class SpringbootDemoApplication extends SpringBootServletInitializer {
 	@Override
-	protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application){
+	protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
 		return application.sources(SpringbootDemoApplication.class);
 	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
