b4c184cc91a925601b4a03eaeb2e697d027986a9
徐鑫波 <769289019@qq.com>
2019-03-16 11:28:47 +0800

    数据库修改 添加service

--- src/main/java/com/zhitu/workshop/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/zhitu/workshop/springbootdemo/SpringbootDemoApplication.java
@@ -3,9 +3,14 @@ package com.zhitu.workshop.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDemoApplication {
+public class SpringbootDemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application){
+		return application.sources(SpringbootDemoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
