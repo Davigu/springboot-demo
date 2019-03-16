614df8e03ba6c990ee1a91f0474e61afc01c9573
wjsdawn <1666938053@qq.com>
2019-03-16 11:21:04 +0800

    实体类修改

--- src/main/java/com/zhitu/workshop/springbootdemo/bo/User.java
+++ src/main/java/com/zhitu/workshop/springbootdemo/bo/User.java
@@ -1,26 +1,102 @@
 package com.zhitu.workshop.springbootdemo.bo;
 
 public class User {
-    private Integer id;
-
-    private String name;
-
+    private Integer userId;
+    private String userName;
     private String email;
-
-    public Integer getId() {
-        return id;
+    private Integer password;
+    private String phone;
+    private String gender;
+    private String auth;
+    private String sign;
+    private Integer ablespace;
+    public Integer getUserId() {
+        return userId;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+
+    public void setAuth(String auth) {
+        this.auth = auth;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public Integer getAblespace() {
+        return ablespace;
+    }
+
+    public void setAblespace(Integer ablespace) {
+        this.ablespace = ablespace;
+    }
+
+
+    public Integer getId() {
+        return userId;
+    }
+
+    public void setId(Integer userId) {
+        this.userId = userId;
     }
 
     public String getName() {
-        return name;
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String userName) {
+        this.userName = userName;
     }
 
     public String getEmail() {
