130dfec6292c2be2d2fe23c3dba79b5fa2595caa
xfchen <xfchen@wisedu.com>
2019-03-17 16:24:22 +0800

    Update ServletInitalizer.java

--- src/main/java/com/zhitu/workshop/springbootdemo/ServletInitalizer.java
+++ src/main/java/com/zhitu/workshop/springbootdemo/ServletInitalizer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitalizer extends SpringBootServletInitializer {
+    // 增加注释
     @Override
     protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
         return application.sources(SpringbootDemoApplication.class);
